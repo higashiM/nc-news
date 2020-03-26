66496765f0391dca1445ebc756bc7d50876e72e8
higashiM <eastwg@gmail.com>
2020-03-26 15:27:06 +0000

    starting to add graphql

--- server/app.js
+++ server/app.js
@@ -3,10 +3,41 @@ const app = express();
 const { customErrors, psqlErrors, otherErrors } = require("./errors/index");
 const { logger } = require("./logger/logger");
 const apiRouter = require("./routers/apiRouter");
-
-const loginRouter = require("./routers/loginRouter");
+const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
+const bodyParser = require("body-parser");
+const { makeExecutableSchema } = require("graphql-tools");
 
 app.use(express.json());
+
+const books = [
+  {
+    title: "Harry Potter and the Sorcerer's stone",
+    author: "J.K. Rowling"
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crichton"
+  }
+];
+
+// The GraphQL schema in string form
+const typeDefs = `
+  type Query { books: [Book] }
+  type Book { title: String, author: String }
+`;
+
+// The resolvers
+const resolvers = {
+  Query: { books: () => books }
+};
+
+// Put together a schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
+app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
 
 app.use("/api", apiRouter);
 
